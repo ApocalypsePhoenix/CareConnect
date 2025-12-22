0c797f9eec24cc6968e916f463fda3558698ba94
isacarcrussell777 <xtreme4mil@gmail.com>
2025-12-22 22:03:10 +0800

    Made changes to Login, Signup for client. Added client dashboard

---
+++ careconnect/php/db_connect.php
@@ -0,0 +1,41 @@
+<?php
+// Database credentials
+$servername = "localhost"; 
+$username = "arcadius_owner"; 
+$password = "R5#8%DOOg[{Y+z7="; 
+$dbname = "arcadius_careconnect";
+
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    public $conn;
+
+    public function __construct() {
+        global $servername, $username, $password, $dbname;
+        $this->host = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db_name = $dbname;
+    }
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            header('Content-Type: application/json');
+            echo json_encode(["success" => false, "message" => "Connection error: " . $exception->getMessage()]);
+            exit;
+        }
+        return $this->conn;
+    }
+}
+?>
